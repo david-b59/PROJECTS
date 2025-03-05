d806324a5a89ac5bbab6402e1f756baf82742761
david-b59 <31815239+david-b59@users.noreply.github.com>
2025-03-05 15:59:52 +0100

    Update app_system_recomand.py
    modif css selectbox

--- project-recommandation-cinema/app_system_recomand.py
+++ project-recommandation-cinema/app_system_recomand.py
@@ -536,11 +536,9 @@ elif tabs == 'Système de recommandation':
 	    <style>
             /* Appliquer le style au selectbox et ses éléments internes */
 	    div[data-baseweb="select"], div[data-baseweb="select"] * {
-	        background-color: #2C3E50 !important; /* Fond bleu-gris foncé */
-	        border: 2px solid #1B2A41 !important; /* Bordure gris foncé */
+	        
 	        border-radius: 8px !important; /* Coins arrondis */
-	        padding: 8px !important; /* Espacement interne */
-	        color: white !important; /* Texte blanc */
+	        
 	        font-family: 'Arial', sans-serif !important; /* Police moderne */
 	        font-size: 16px !important; /* Texte plus grand */
 	    }
